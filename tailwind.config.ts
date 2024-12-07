f18dbc2cf9311490a69aeba48efb1c1eead9e761
soyeon <csea9000@naver.com>
2024-12-07 14:13:12 +0900

    fix: warning값 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,7 @@ const config: Config = {
         gray02: "#4A4A4C",
         gray03: "#CED1D6",
         gray04: "#F5F5F6",
+        warning: "#FF3E60",
       },
       width: {
         layout: "var(--min-layout-size)",
