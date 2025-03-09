2dadf5884cd7c5fe7f8f4b3687e335909585a975
soyeon102 <csea9000@naver.com>
2025-03-09 12:28:52 +0000

    fix: 모바일 확인을 위한 CORS 에러 수정 (프록시 설정)

--- src/constants/url.const.ts
+++ src/constants/url.const.ts
@@ -1,2 +1,5 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_URL =
+  process.env.NODE_ENV === "development"
+    ? "/api"
+    : process.env.NEXT_PUBLIC_API_URL;
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
