dfb26e3e53320d693752be89631a6c054a437ed6
soyeon102 <csea9000@naver.com>
2025-03-09 21:25:56 +0900

    fix: 모바일 확인을 위한 CORS 에러 수정 (프록시 설정)

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,14 @@ const nextConfig = {
       { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
